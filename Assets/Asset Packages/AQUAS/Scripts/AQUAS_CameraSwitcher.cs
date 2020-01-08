c7853c4c8960a9128fdd6c2653b3112989b9d99d
hectorborges <38707692+hectorborges@users.noreply.github.com>
2020-01-08 11:06:31 -0600

    added the light the hoan environment

---
+++ Assets/Asset Packages/AQUAS/Scripts/AQUAS_CameraSwitcher.cs
@@ -0,0 +1,27 @@
+﻿#if UNITY_EDITOR
+
+using UnityEngine;
+using UnityEditor;
+#if UNITY_POST_PROCESSING_STACK_V1 && !UNITY_POST_PROCESSING_STACK_V2 && AQUAS_PRESENT
+using UnityEngine.PostProcessing;
+#endif
+
+
+public class AQUAS_CameraSwitcher : MonoBehaviour {
+
+    public GameObject camerappv1;
+    public GameObject camerappv2;
+
+	// Use this for initialization
+	void Awake () {
+
+#if AQUAS_PRESENT && UNITY_POST_PROCESSING_STACK_V1
+        camerappv2.SetActive(false);
+        camerappv1.SetActive(true);
+#endif
+
+
+    }
+}
+
+#endif
