1d089eeeb21b5cb87c797699adfea4b5a7a31f33
hectorborges <38707692+hectorborges@users.noreply.github.com>
2020-01-21 17:25:09 -0600

    testing gaunlet

---
+++ Assets/Gaunlet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gaunlet : MonoBehaviour
+{
+    public LineRenderer line;
+    public LayerMask mask;
+
+    private void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 100f, mask))
+        {
+            Debug.Log("Hit Target");
+            line.enabled = true;
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, hit.point);
+        }
+        else
+            line.enabled = false;
+    }
+}
