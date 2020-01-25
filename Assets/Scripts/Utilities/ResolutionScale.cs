e0fdec263c6c64efab4f4794b45b0c7f15f0299d
hectorborges <hectorborges94@gmail.com>
2020-01-24 20:30:40 -0600

    commit 5

---
+++ Assets/Scripts/Utilities/ResolutionScale.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+public class ResolutionScale : MonoBehaviour
+{
+    public float resolutionScale = 1;
+    private void Awake()
+    {
+        XRSettings.eyeTextureResolutionScale = resolutionScale;
+    }
+}
