28e017714db03d81270c48f9857b0f19ae1a02f7
hectorborges <hectorborges94@gmail.com>
2020-02-25 14:21:42 -0600

    Added THECode to THECodeTestingBranch

---
+++ Assets/Scripts/Effects/ButtonEffects.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class ButtonEffects : Effect
+{
+    //on the unity editor
+    //populate enums based upon 
+
+
+
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AutomateIt() { }
+
+    IEnumerator DoCoroutinesWork() { yield return new WaitForSeconds(3.14f); }
+}
---
+++ Assets/Scripts/Effects/Effect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Effect : MonoBehaviour
+{
+    public Effect[] effects;
+    public enum EffectTypes { };
+
+    EffectTypes effectType;
+
+    public string[] methodNames = { "name", "anotherName", "oneMoreName"};
+
+    protected virtual void Awake()
+    {
+
+
+
+        Type myType = (typeof(ButtonEffects));
+        MethodInfo[] myArrayMethodInfo = myType.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+        for (int i = 0; i < myArrayMethodInfo.Length; i++)
+        {
+            MethodInfo myMethodInfo = (MethodInfo)myArrayMethodInfo[i];
+           // effectType = methodNames;
+
+           // methodNames.Add(myMethodInfo.Name);
+        }
+
+    }
+
+    public virtual void InvokeEffect() { }
+}
